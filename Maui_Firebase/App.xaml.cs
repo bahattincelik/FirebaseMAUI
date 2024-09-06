6cca7798432e4844b26e470e458b101cb25810f9
Bahattin Celik <celik@wave-solutions.de>
2024-09-06 10:28:10 +0200

    first

---
+++ Maui_Firebase/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_Firebase
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui_Firebase/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Maui_Firebase
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_ClickedAnmelden(object sender, EventArgs e)
+        {
+            if (SLEntry.IsVisible)
+            {
+                SLEntry.IsVisible = false;
+                SLAnmelden.IsVisible = true;
+            }
+            else 
+            { 
+                SLEntry.IsVisible = true;
+                SLAnmelden.IsVisible=false;
+            }
+        }
+    }
+
+}
